77dc07f3884dee87a22e5a96f8a79e72e3b19671
andrewlngdn <andrew@alpinenow.com>
2015-05-08 15:41:38 -0700

    enforce role.users uniqueness

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -6,5 +6,13 @@ describe Role do
     it { should have_and_belong_to_many(:users) }
     it { should have_and_belong_to_many(:groups) }
     it { should have_many(:chorus_objects).through(:chorus_object_roles) }
+
+    let (:user) { users(:admin) }
+    let (:role) { roles(:a_role) }
+    it "should not allow dupliacte users" do
+      role.users << user
+      expect{ role.users << user }.to_not change{ role.users.count }.from(1).to(2)
+    end
+
   end
 end
