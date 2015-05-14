1c2f3e11e303192a0a33d5b47dcf7a42c9bbdbce
andrewlngdn <andrew@alpinenow.com>
2015-05-13 17:43:43 -0700

    updated members controller to use new authentication system

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,14 +1,15 @@
 class MembersController < ApplicationController
   def index
     workspace = Workspace.find(params[:workspace_id])
-    authorize! :show, workspace
+    Authority.authorize! :show, workspace, current_user, { :or => [ :current_user_is_in_workspace,
+                                                                    :workspace_is_public ] }
 
     present paginate WorkspaceAccess.members_for(current_user, workspace)
   end
 
   def create
     workspace = Workspace.find(params[:workspace_id])
-    authorize! :owner, workspace
+    Authority.authorize! :show, workspace, current_user, { :or => :current_user_is_workspace_owner }
 
     WorkspaceMembersManager.new(
         workspace,
