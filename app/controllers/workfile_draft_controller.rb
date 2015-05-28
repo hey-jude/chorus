936e02c1754c7555097a4d08b83c748074972d8f
andrewlngdn <andrew@alpinenow.com>
2015-05-28 14:33:35 -0700

    update workfile copy, download, draft, to use new authorize system

--- app/controllers/workfile_draft_controller.rb
+++ app/controllers/workfile_draft_controller.rb
@@ -1,7 +1,9 @@
 class WorkfileDraftController < ApplicationController
+  before_filter :find_workfile
+  before_filter :authorize_sub_objects, :only => [:create, :update, :destroy]
   def create
-    workfile = Workfile.find(params[:workfile_id])
-    authorize! :can_edit_sub_objects, workfile.workspace
+    #workfile = Workfile.find(params[:workfile_id])
+    #Authority.authorize! :update, workspace, current_user, { :or => :can_edit_sub_objects }
 
     draft = WorkfileDraft.new(params[:workfile_draft])
     draft.workfile_id = params[:workfile_id]
@@ -11,16 +13,20 @@ class WorkfileDraftController < ApplicationController
   end
 
   def show
-    workfile = Workfile.find(params[:workfile_id])
-    authorize! :show, workfile.workspace
-
+    #workfile = Workfile.find(params[:workfile_id])
+    #authorize! :show, workfile.workspace
+    Authority.authorize! :show,
+                         @workfile.workspace,
+                         current_user,
+                         { :or => [ :current_user_is_in_workspace,
+                                    :workspace_is_public ] }
     draft = WorkfileDraft.find_by_owner_id_and_workfile_id!(current_user.id, params[:workfile_id])
     present draft
   end
 
   def update
-    workfile = Workfile.find(params[:workfile_id])
-    authorize! :can_edit_sub_objects, workfile.workspace
+    #workfile = Workfile.find(params[:workfile_id])
+    #authorize! :can_edit_sub_objects, workfile.workspace
 
     draft = WorkfileDraft.find_by_owner_id_and_workfile_id!(current_user.id, params[:workfile_id])
     draft.update_attributes!(params[:workfile_draft])
@@ -28,11 +34,21 @@ class WorkfileDraftController < ApplicationController
   end
 
   def destroy
-    workfile = Workfile.find(params[:workfile_id])
-    authorize! :can_edit_sub_objects, workfile.workspace
+    #workfile = Workfile.find(params[:workfile_id])
+    #authorize! :can_edit_sub_objects, workfile.workspace
 
     draft = WorkfileDraft.find_by_owner_id_and_workfile_id!(current_user.id, params[:workfile_id])
     draft.destroy
     render :json => {}
   end
+
+
+  def find_workfile
+    @workfile = Workfile.find(params[:workfile_id])
+  end
+
+  def authorize_sub_objects
+    Authority.authorize! :update, @workfile.workspace, current_user, { :or => :can_edit_sub_objects }
+  end
+
 end
