cd0803c0e3dd6fac36e542077e2834d41dc7814d
andrewlngdn <andrew@alpinenow.com>
2015-06-30 17:49:33 -0700

    DEV-11544 remove old allowy access files

--- app/permissions/gnip_data_source_access.rb
+++
@@ -1,5 +0,0 @@
-class GnipDataSourceAccess < AdminFullAccess
-  def show?(gnip_data_source)
-    true
-  end
-end
--- app/permissions/gpdb_view_access.rb
+++
@@ -1,5 +0,0 @@
-class GpdbViewAccess < AdminFullAccess
-  def show?(gpdb_view)
-    GpdbDataSourceAccess.new(context).can? :show, gpdb_view.data_source
-  end
-end
