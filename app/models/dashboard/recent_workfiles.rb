e8349887477b416655b4a4186e10a815ff600270
prakash-alpine <prakash@alpinenow.com>
2015-07-09 14:01:07 -0400

    Added filtering query result by user's current scope

--- app/models/dashboard/recent_workfiles.rb
+++ app/models/dashboard/recent_workfiles.rb
@@ -14,18 +14,20 @@ module Dashboard
       if limitValue == ''
         limitValue = 5
       end
-
-      workfiles =  OpenWorkfileEvent.
+      workfiles = nil
+      active_rel =  OpenWorkfileEvent.
           select('max(created_at) as created_at, workfile_id').
           where(:user_id => user.id).
           group(:workfile_id).
           order('created_at DESC').
           includes(:workfile).
           limit(limitValue)
-
+      if active_rel != nil
+        workfiles = active_rel.all
+      end
       if Permissioner.user_in_scope? @user
         # PTELI:SCOPE Filter results by scope for current user
-        OpenWorkfileEvent.filter_by_scope(@user, workfiles)
+        workfiles = OpenWorkfileEvent.filter_by_scope(@user, workfiles)
       else
         workfiles
       end
