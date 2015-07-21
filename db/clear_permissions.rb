bb556210774a06d4cc8cf1c2c2454dfd37c8f4dd
andrewlngdn <andrew@alpinenow.com>
2015-07-21 15:58:18 -0700

    DEV-11768 add SQL delete statements to remove join tables when running clear_permissions

--- db/clear_permissions.rb
+++ db/clear_permissions.rb
@@ -1,13 +1,32 @@
 # Clear all tables related to roles, groups and permissions.
 puts 'Clearing data entries for roles, groups and permissions'
-Role.destroy_all
-Group.destroy_all
-Permission.destroy_all
-ChorusScope.destroy_all
-ChorusClass.destroy_all
-Operation.destroy_all
-ChorusObject.delete_all
-ChorusObjectRole.destroy_all
+connection = ActiveRecord::Base.connection
+#Role.destroy_all
+connection.execute("DELETE FROM roles")
+connection.execute("DELETE FROM roles_users")
+
+#Group.destroy_all
+connection.execute("DELETE FROM groups")
+connection.execute("DELETE FROM groups_roles")
+connection.execute("DELETE FROM groups_users")
+
+#Permission.destroy_all
+connection.execute("DELETE FROM permissions")
+
+#ChorusScope.destroy_all
+connection.execute("DELETE FROM chorus_scopes")
+
+#Operation.destroy_all
+connection.execute("DELETE FROM operations")
+
+#ChorusClass.destroy_all
+connection.execute("DELETE FROM chorus_classes")
+
+#ChorusObject.destroy_all
+connection.execute("DELETE FROM chorus_objects")
+
+#ChorusObjectRole.destroy_all
+connection.execute("DELETE FROM chorus_object_roles")
 puts '--- DONE ----'
 
 
