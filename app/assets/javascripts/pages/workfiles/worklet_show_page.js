b9dc27fa78b1746ba443639431faff1d5c2b2d95
Michael Souza <mike@michaelsouza.com>
2015-07-26 22:09:45 -0700

    Worklets: in workspace mode there are 'edit' and 'preview' modes (pages).

--- app/assets/javascripts/pages/workfiles/worklet_show_page.js
+++ app/assets/javascripts/pages/workfiles/worklet_show_page.js
@@ -1,20 +1,30 @@
-chorus.pages.WorkletShowPage = chorus.pages.Base.extend({
+chorus.pages.WorkletWorkspaceDisplayBase = chorus.pages.Base.extend({
     helpId: "workfile",
 
-    setup:function (workspaceId, workletId) {
-        this.worklet = new chorus.models.Worklet({id: workletId, workspace: this.workspace});
-        this.worklet.fetch();
-        this.onceLoaded(this.worklet, this.buildPage);
-
+    setup: function (workspaceId, workletId) {
+        this.subNav = new chorus.views.SubNav({workspace: this.workspace, tab: "workfiles"});
         this.requiredResources.add(this.workspace);
 
-        this.subNav = new chorus.views.SubNav({workspace: this.workspace, tab: "workfiles"});
-
         this.subscribePageEvent("menu:worklet", this.menuEventHandler);
+
+        this.worklet = new chorus.models.Worklet({id: workletId, workspace: this.workspace});
+        this.worklet.fetch();
+
+        this.onceLoaded(this.worklet, this.buildPage);
     },
 
     makeModel: function(workspaceId) {
         this.loadWorkspace(workspaceId);
+    },
+
+    menuEventHandler: function(menu_item) {
+        if (menu_item === 'edit_mode') {
+            chorus.router.navigate(this.worklet.showUrl());
+        }
+
+        if (menu_item === 'preview_mode') {
+            chorus.router.navigate(this.worklet.showUrl() + '/preview');
+        }
     },
 
     updateMiddleContent: function(newView) {
@@ -26,6 +36,11 @@ chorus.pages.WorkletShowPage = chorus.pages.Base.extend({
         this.mainContent.content.renderSubview('workletOutput');
 
         this.trigger('resized');
+    },
+
+    hideHistory: function() {
+        $(this.mainContent.content.workletHistory.el).toggle(false);
+        $('.published_worklet_content').addClass('no_history');
     },
 
     showHistory: function() {
@@ -44,8 +59,58 @@ chorus.pages.WorkletShowPage = chorus.pages.Base.extend({
         this.mainContent.content.workletHistory = newView;
         this.mainContent.content.workletSubmit.historyView = newView;
         this.mainContent.content.renderSubview('workletHistory');
-        this.trigger('resized');
 
+        $('.published_worklet_content').removeClass('no_history');
+        this.trigger('resized');
+    }
+});
+
+chorus.pages.WorkletPreviewPage = chorus.pages.WorkletWorkspaceDisplayBase.extend({
+    //menuEventHandler: function(menu_item) {
+    //    this._super("menuEventHandler", [menu_item]);
+    //},
+
+    buildPage: function() {
+        this.history = this.worklet.activities({resultsOnly: true, currentUserOnly: true});
+        this.history.fetchAll();
+        this.onceLoaded(this.history, this.showHistory);
+
+        this.headerView = new chorus.views.WorkletHeader({
+            model: this.worklet,
+            menuOptions: [],
+            mode: 'preview'
+        });
+
+        this.contentView = new chorus.views.PublishedWorkletContent({
+            model: this.worklet,
+            collection: this.history
+        });
+
+        this.mainContent = new chorus.views.MainContentView({
+            model: this.model,
+            contentHeader: this.headerView,
+            content: this.contentView
+        });
+
+        this.render();
+
+        // Show history
+        // $(this.mainContent.content.workletSubmit.el).find('.form_controls').toggle(true);
+        // this.showHistory();
+    }
+});
+
+chorus.pages.WorkletEditPage = chorus.pages.WorkletWorkspaceDisplayBase.extend({
+    menuEventHandler: function(menu_item) {
+        this._super("menuEventHandler", [menu_item]);
+
+        if (menu_item === 'details') {
+            this.showDetailsConfiguration();
+        }
+
+        if (menu_item === 'inputs') {
+            this.showInputsConfiguration();
+        }
     },
 
     showDetailsConfiguration: function() {
@@ -67,14 +132,9 @@ chorus.pages.WorkletShowPage = chorus.pages.Base.extend({
         this.updateMiddleContent(inputs_configuration);
     },
 
-    menuEventHandler: function(menu_item) {
-        if (menu_item === 'details') {
-            this.showDetailsConfiguration();
-        }
-
-        if (menu_item === 'inputs') {
-            this.showInputsConfiguration();
-        }
+    hidePreviewControls: function() {
+        this.hideHistory();
+        $(this.mainContent.content.workletSubmit.el).find('.form_controls').toggle(false);
     },
 
     buildPage: function() {
@@ -86,10 +146,6 @@ chorus.pages.WorkletShowPage = chorus.pages.Base.extend({
         this.workflowOperators.fetch();
         this.onceLoaded(this.workflowOperators, this.showDetailsConfiguration);
 
-        this.history = this.worklet.activities({resultsOnly: true, currentUserOnly: true});
-        this.history.fetchAll();
-        this.onceLoaded(this.history, this.showHistory);
-
         //this.workflow = new chorus.models.AlpineWorkfile({id: this.worklet.workflow_id, workspace: this.workspace});
         //this.workflow.fetch();
         //this.onceLoaded(this.workflow, function (e) {
@@ -97,7 +153,12 @@ chorus.pages.WorkletShowPage = chorus.pages.Base.extend({
         //});
 
         this.headerView = new chorus.views.WorkletHeader({
-            model: this.worklet
+            model: this.worklet,
+            menuOptions: [
+                {data: "details", text: "Configure Details"}, //t("workfiles.header.menu.sort.by_date")
+                {data: "inputs", text: "Configure Inputs"}
+            ],
+            mode: 'edit'
         });
 
         this.contentView = new chorus.views.PublishedWorkletContent({
@@ -112,5 +173,6 @@ chorus.pages.WorkletShowPage = chorus.pages.Base.extend({
         });
 
         this.render();
+        this.hidePreviewControls();
     }
 });
