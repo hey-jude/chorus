8da84edbcc24d0c83119163819a2cac1c50652ed
Kevin Trowbridge <kevinmtrowbridge@gmail.com>
2015-08-05 15:59:01 -0700

    DEV-11947: worklets catalog: implement catalog page Making the pagination work properly.

--- app/assets/javascripts/views/workfiles/published_worklet_list_view.js
+++ app/assets/javascripts/views/workfiles/published_worklet_list_view.js
@@ -2,12 +2,6 @@ chorus.views.PublishedWorkletList = chorus.views.Base.extend({
     constructorName: "PublishedWorkletListView",
     templateName: "published_worklet_list",
 
-    additionalContext: function() {
-        return {
-            emptyListTranslationKey: "entity.name.PublishedWorklet.none"
-        };
-    },
-
     collectionModelContext: function (model) {
         return {
             url: model.showUrl()
--- app/controllers/published_worklet_controller.rb
+++ app/controllers/published_worklet_controller.rb
@@ -11,9 +11,9 @@ class PublishedWorkletController < ApplicationController
     published_worklets = PublishedWorklet.where("additional_data LIKE '%\"state\":\"published\"%'")
 
     published_worklets = published_worklets.order_by(params[:order]).includes(:latest_workfile_version)
-    published_worklets = published_worklets.where("workfiles.file_name LIKE ?", "%#{params[:name_pattern]}%") if params[:name_pattern]
+    published_worklets = published_worklets.where("workfiles.file_name ILIKE ?", "%#{params[:name_pattern]}%") if params[:name_pattern]
 
-    present published_worklets
+    present paginate(published_worklets), :presenter_options => {:cached => true, :namespace => "published_worklets"}
   end
 
   def show
