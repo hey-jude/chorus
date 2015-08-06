edb8084b948e19c36506be2afc35a343020eba5f
Kevin Trowbridge <kevinmtrowbridge@gmail.com>
2015-08-06 11:54:41 -0700

    DEV-11900 Help and Support dialog re-routes you to the Home page when invoked
    @ggao rejected my previous fix, saying: "There is still one issue, if
    user open a flow in Alpine and try to open the Help and Support . it
    will go back to home again"
    
    ... after investigation, I need to slap an e.stopPropagation() on there,
    too.
    
    #bugfix #js_events_related #alpine_integration

--- app/assets/javascripts/views/layout/header.js
+++ app/assets/javascripts/views/layout/header.js
@@ -218,7 +218,7 @@ chorus.views.Header = chorus.views.Base.extend({
     },
 
     helpAndSupport: function(e){
-        e.preventDefault();
+        e.preventDefault() && e.stopPropogation();
         this.dialog = new chorus.dialogs.HelpAndSupport({ model: this.model });
         this.dialog.launchModal();
     }
