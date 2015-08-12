6fe710ae59001764ac08494fb26e2a762e1cd380
Jason Lee <jlee@alpinedatalabs.com>
2015-08-11 18:22:04 -0700

    DEV-12075 DEV-12070 - preventing caching by presenting results of associated worklets in workfile_presenter.rb - "worklet usage" dialog links now take you to the correct worklet

--- app/presenters/workfile_presenter.rb
+++ app/presenters/workfile_presenter.rb
@@ -25,7 +25,7 @@ class WorkfilePresenter < Presenter
     end
 
     workfile.merge!({
-        :associated_worklets => Worklet.where("additional_data LIKE '%\"workflow_id\":" + model.id.to_s + "%' AND content_type='worklet'").order(:file_name)
+        :associated_worklets => present(Worklet.where("additional_data LIKE '%\"workflow_id\":" + model.id.to_s + "%' AND content_type='worklet'").order(:file_name))
     })
 
     workfile
