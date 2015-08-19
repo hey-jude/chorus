ab9fc8e348bc679aa215ff4c46339190ba41c9f4
Jason Lee <jlee@alpinedatalabs.com>
2015-08-19 01:26:14 -0700

    checking that should address several blocker issues involving running touchpoints as collaborators and running concurrent touchpoints - DEV-12423, DEV-12429, DEV-12413, DEV-12417, DEV-12428 - this fixes a few issues that prevent collaborators from running touchpoints (front-end permissions issues), as well as functionality that ensures that the proper data source information is copied over to the right place when a touchpoint is set to "run as creator"

--- app/assets/javascripts/models/published_worklet.js
+++ app/assets/javascripts/models/published_worklet.js
@@ -28,6 +28,11 @@ chorus.models.PublishedWorklet = chorus.models.Worklet.include(
         entitySubtype: "published_worklet"
     },
 
+    // This is necessary to make sure collaborators can run a published worklet
+    canEdit: function() {
+        return this.isLatestVersion() && this.workspace().isActive();
+    },
+
     run: function(worklet_parameters) {
         this.save({worklet_parameters: worklet_parameters}, {
             workflow_action: 'run',
