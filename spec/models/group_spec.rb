f81d087f99b77997d4ed0f8f1c8afc30ddbea64d
andrewlngdn <andrew@alpinenow.com>
2015-08-25 16:04:32 -0700

    update association check in group spec

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Group do
   describe "associations" do
     it { should have_and_belong_to_many(:users) }
-    it { should have_one(:chorus_scope) }
+    it { should have_and_belong_to_many(:chorus_scopes) }
     it { should have_and_belong_to_many(:roles) }
   end
 end
