842c111742873248a647f320e32b44f27ecee9e5
andrewlngdn <andrew@alpinenow.com>
2015-08-25 15:55:15 -0700

    update scope association check

--- spec/models/chorus_scope_spec.rb
+++ spec/models/chorus_scope_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ChorusScope do
   describe "associations" do
-    it { should belong_to(:group) }
+    it { should have_and_belong_to_many(:groups) }
     it { should have_many(:chorus_objects) }
   end
 end
