d85927f774e3fd8d9663c30d34bd6c96c8e902d8
andrewlngdn <andrew@alpinenow.com>
2015-08-26 16:11:44 -0700

    update permissioner spec to test model scoping

--- spec/support/shared_behaviors/permissioner_shared_behaviors.rb
+++ spec/support/shared_behaviors/permissioner_shared_behaviors.rb
@@ -36,8 +36,8 @@ shared_examples "a permissioned model" do
     end
 
     it "provides a scope filter for a collection of objects" do
-      #expect(model.class.filter_by_scope(user_a, model.class.all)).to eq([model_a])
-      #expect(model.class.filter_by_scope(user_b, model.class.all)).to eq([model_b])
+      expect(model.class.filter_by_scope(user_a, model.class.all)).to eq([model_a])
+      expect(model.class.filter_by_scope(user_b, model.class.all)).to eq([model_b])
     end
   end
 
@@ -49,40 +49,6 @@ shared_examples "a permissioned model" do
     expect(chorus_object).to_not be_nil
   end
 
-  # it "initializes the default roles if they exist" do
-  #   next if !model.class.const_defined? 'OBJECT_LEVEL_ROLES' # Some permissioned objects don't use object level roles
-  #
-  #   object_roles_symbols = model.class::OBJECT_LEVEL_ROLES
-  #   object_roles = model.object_roles
-  #   symbols = object_roles.map {|role| role.name.to_sym }
-  #
-  #   expect(object_roles_symbols).to eq(symbols)
-  # end
 
-  #describe "when adding permissions" do
-  #  let (:role) { roles(:a_role) }
-  #  let (:permission) { model.class::PERMISSIONS.first }
-  #
-  #  it "should create .permissions on the chorus class" do
-  #    old_count = ChorusClass.find_by_name(model.class.name).permissions.count
-  #    model.class.set_permissions_for(role, permission)
-  #    new_count = ChorusClass.find_by_name(model.class.name).permissions.count
-  #
-  #    expect(new_count).to eq(old_count + 1)
-  #  end
-  #
-  #
-  #end
-
-  #describe "permission_symbols_for" do
-  #  let (:role) { roles(:a_role) }
-  #  let (:user) { User.new }
-  #  let (:permission) { model.class::PERMISSIONS.first }
-  #  it "should_return the correct permission_symbol" do
-  #    user.roles << role
-  #    model.class.set_permissions_for(role, permission)
-  #    expect(model.class.permission_symbols_for(user)).to eq(Array.wrap(permission))
-  #  end
-  #end
 
 end
