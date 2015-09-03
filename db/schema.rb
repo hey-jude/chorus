eb6d1a44ec2c3201add65abe22134095480f6df0
Kevin Trowbridge <kevinmtrowbridge@gmail.com>
2015-09-02 19:15:00 -0700

    Updating some generated files to get around the "check_clean_working_tree" part of the packaging scripts.
    #rails4_upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150731154600) do
+ActiveRecord::Schema.define(version: 20150811080749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -546,6 +546,7 @@ ActiveRecord::Schema.define(version: 20150731154600) do
     t.datetime "deleted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status"
   end
 
   create_table "schemas", force: true do |t|
@@ -709,6 +710,10 @@ ActiveRecord::Schema.define(version: 20150731154600) do
     t.datetime "user_modified_at"
     t.text     "execution_location_type"
     t.string   "status",                     default: "idle"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "workfiles", ["owner_id"], name: "index_workfiles_on_owner_id", using: :btree
