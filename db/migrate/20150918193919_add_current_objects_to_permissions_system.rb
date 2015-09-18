13021523108cac950e6b9f79bb5e225c7cfd1cdd
andrewlngdn <andrew@alpinenow.com>
2015-09-18 14:34:49 -0700

    fix double include error and update migration

--- db/migrate/20150918193919_add_current_objects_to_permissions_system.rb
+++ db/migrate/20150918193919_add_current_objects_to_permissions_system.rb
@@ -3,8 +3,6 @@ class AddCurrentObjectsToPermissionsSystem < ActiveRecord::Migration
     ChorusObject.delete_all
     ChorusObjectRole.delete_all
 
-    User # This line is surprisingly necessary. Without it, Rails will incorrectly think that
-         # the User class is double 'include'ing inside its Concern
     PermissionsMigrator.migrate_5_7
   end
 end
