86ebf65acc1e383a51ff4a390c733881bbf7a53c
andrewlngdn <andrew@alpinenow.com>
2015-09-21 14:32:43 -0700

    remove rails 3-specific code

--- components/admin/lib/admin/engine.rb
+++ components/admin/lib/admin/engine.rb
@@ -5,11 +5,5 @@ require 'jquery-ui-rails'
 module Admin
   class Engine < ::Rails::Engine
     isolate_namespace Admin
-
-    # Initializer to combine this engines static assets with the static assets of the hosting site.
-    initializer 'static assets' do |app|
-      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
-    end
-
   end
 end
