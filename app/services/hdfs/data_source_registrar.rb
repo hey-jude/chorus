8239d26d09c1668e3c9567a0ec18ad34ee8d9e2f
andrewlngdn <andrew@alpinenow.com>
2015-10-06 12:48:45 -0700

    DEV-13005 update hdfs backend to allow for incomplete data sources to be saved

--- app/services/hdfs/data_source_registrar.rb
+++ app/services/hdfs/data_source_registrar.rb
@@ -3,9 +3,15 @@ module Hdfs
     def self.create!(connection_config, owner)
       data_source = HdfsDataSource.new(connection_config)
       data_source.owner = owner
-      verify_accessibility!(data_source)
-      data_source.version = Hdfs::QueryService.version_of(data_source)
-      data_source.save!
+
+      if connection_config['state'] == 'incomplete'
+        data_source.save!(:validate => false)
+      else
+        verify_accessibility!(data_source)
+        data_source.version = Hdfs::QueryService.version_of(data_source)
+        data_source.save!
+      end
+
       Events::HdfsDataSourceCreated.by(owner).add(:hdfs_data_source => data_source)
       data_source
     end
@@ -13,8 +19,11 @@ module Hdfs
     def self.update!(data_source_id, connection_config, updater)
       data_source = HdfsDataSource.find(data_source_id)
       data_source.attributes = connection_config.except(:version)
-      data_source.version = Hdfs::QueryService.version_of(data_source)
-      verify_accessibility!(data_source)
+
+      unless (connection_config['state'] == 'incomplete')
+        data_source.version = Hdfs::QueryService.version_of(data_source)
+        verify_accessibility!(data_source)
+      end
 
       if data_source.name_changed? && data_source.valid?
         Events::HdfsDataSourceChangedName.by(updater).add(
@@ -24,9 +33,15 @@ module Hdfs
         )
       end
 
-      data_source.save!
+      if connection_config['state'] == 'incomplete'
+        data_source.save!(:validate => false)
+      else
+        data_source.save!
+      end
+
       data_source
     end
+
 
     def self.verify_accessibility!(data_source)
       unless Hdfs::QueryService.accessible?(data_source)
