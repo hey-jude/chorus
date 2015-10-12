68ff4bc1e7801b92295299c4acbc3923970792eb
Michael Souza <mike@michaelsouza.com>
2015-10-12 15:26:05 -0700

    DEV-12769, DEV-12990, DEV-13021 - Most active workspaces widget sorting bug fix.

--- app/models/dashboard/workspace_activity.rb
+++ app/models/dashboard/workspace_activity.rb
@@ -96,8 +96,9 @@ module Dashboard
                :start_date => @start_date,
                :end_date => Time.now)
       .count
+      events_by_datepart_workspace = Hash[events_by_datepart_workspace.map { |k,v| [[k[0], k[1].strftime('%F 00:00:00 UTC')], v] }]
 
-      # Fill in gaps and construct axis labels
+      # Fill in date gaps (each date in the range should be present) and construct axis labels
       labels = []
       (0..@date_parts).each do |d|
         label = @rules[@date_group][:label_fcn].call(@rules[@date_group][:history_fcn].call(d))
@@ -109,16 +110,14 @@ module Dashboard
           labels << label
         end
 
-        fmt_d = @rules[@date_group][:history_fcn].call(d).strftime('%F 00:00:00')
+        fmt_d = @rules[@date_group][:history_fcn].call(d).strftime('%F 00:00:00 UTC')
         top_workspace_ids.each do |id|
           events_by_datepart_workspace[[id, fmt_d]] ||= 0
         end
       end
 
       # Sort by date
-      # Prakash (5/24). Need to append "to_s" to date string for strptime method to work. It is throwing exception otherwise.
-      # Not sure how it worked earlier in Rails 3.2
-      events_by_datepart_workspace = events_by_datepart_workspace.sort_by { |k,v| Date.strptime(k.last.to_s, '%F %T') }
+      events_by_datepart_workspace = events_by_datepart_workspace.sort_by { |k,v| Date.strptime(k.last, '%F %T') }
       evs = events_by_datepart_workspace.map do |t, v|
         {
           date_part: t.last.to_s[0..."YYYY-MM-DD".length],
