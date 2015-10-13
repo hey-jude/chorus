2350f00a14797d33413528ac3fdd3df5b9dd5b22
andrewlngdn <andrew@alpinenow.com>
2015-10-13 15:51:41 -0700

    DEV-13136 update OpenWorkfileEvents to delete old workfile events on create

--- app/models/open_workfile_event.rb
+++ app/models/open_workfile_event.rb
@@ -5,4 +5,18 @@ class OpenWorkfileEvent < ActiveRecord::Base
 
   attr_accessible :user, :workfile
   validates_presence_of :user, :workfile
+
+  after_create :delete_old_events_for_user
+
+  EVENT_LIMIT = 15
+
+  def delete_old_events_for_user
+    number_of_events = OpenWorkfileEvent.where(:user_id => self.user_id).count
+
+    if number_of_events > EVENT_LIMIT
+      oldest_event_id = OpenWorkfileEvent.where('user_id = ?', user_id).order('created_at DESC').limit(EVENT_LIMIT).last.id
+      old_events = OpenWorkfileEvent.where("id < ? AND user_id = ?", oldest_event_id, self.user_id)
+      old_events.destroy_all
+    end
+  end
 end
