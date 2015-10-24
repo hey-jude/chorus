6037be0d592018e9287bf313d90aed5209f4c009
andrewlngdn <andrew@alpinenow.com>
2015-10-23 17:45:35 -0700

    DEV-13047 update schemas controller

--- app/controllers/schemas_controller.rb
+++ app/controllers/schemas_controller.rb
@@ -4,7 +4,7 @@ class SchemasController < ApplicationController
   def show
     schema = Schema.find(params[:id])
     authorize_data_source_access(schema)
-    raise Authority::AccessDenied.new("Forbidden", :data_source, nil) if schema.data_source == 'disabled'
+    raise Authority::AccessDenied.new("Forbidden", :data_source, nil) if schema.data_source.state == 'disabled'
     schema.verify_in_source(current_user)
 
     present schema
