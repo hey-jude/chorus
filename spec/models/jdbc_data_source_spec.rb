88f75786c778bd318dc740489d02333afc1ce7a7
andrewlngdn <andrew@alpinenow.com>
2015-11-05 12:17:40 -0800

    delete bad test

--- spec/models/jdbc_data_source_spec.rb
+++ spec/models/jdbc_data_source_spec.rb
@@ -5,14 +5,6 @@ describe JdbcDataSource do
 
   describe 'validations' do
     it { should validate_presence_of(:host) }
-
-    context 'when creating' do
-      let(:data_source) { FactoryGirl.build(:jdbc_data_source) }
-      it 'validates the owner account' do
-        mock(data_source).owner_account { mock(FactoryGirl.build(:data_source_account)).valid? { true } }
-        data_source.valid?
-      end
-    end
   end
 
   describe 'destroy' do
