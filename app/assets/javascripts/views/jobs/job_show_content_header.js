1d9b40ad142e5b49602fb9adc9edb2b295daee0d
Michel Milano <mmilano@alpinenow.com>
2015-11-05 12:34:56 -0800

    revise job page to display the job icon at top, while adjusting the headers in app

--- app/assets/javascripts/views/jobs/job_show_content_header.js
+++ app/assets/javascripts/views/jobs/job_show_content_header.js
@@ -2,13 +2,25 @@ chorus.views.JobShowContentHeader = chorus.views.Base.extend({
     constructorName: "JobShowContentHeader",
     templateName: "job_show_content_header",
 
+    jobsIconPath: "/images/jobs/",
+    jobIconActive: "job.svg",
+    jobIconDisabled: "job-disabled.svg",
+
     additionalContext: function () {
         return {
+            iconUrl: this.iconUrl(),
             frequency: this.model.frequency(),
             lastRunStatusKey: this.model.lastRunStatusKey(),
             lastRunLinkKey: this.model.lastRunLinkKey(),
             ownerName: this.model.owner().displayName(),
             ownerUrl: this.model.owner().showUrl()
         };
-    }
+    },
+    
+    iconUrl: function () {
+        // job entity icon
+        var icon = (this.model.get('enabled') || this.model.runsOnDemand()) ? this.jobIconActive : this.jobIconDisabled;
+        return this.jobsIconPath + icon;
+    },
+    
 });
