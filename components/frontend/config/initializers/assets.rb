1c8a190f1e79167f4140db7b95ba5e43a74ad007
Kevin Trowbridge <kevinmtrowbridge@gmail.com>
2015-11-11 17:07:05 -0800

    Fixing up asset pipeline in production ...
    #componentization

---
+++ components/frontend/config/initializers/assets.rb
@@ -0,0 +1,7 @@
+Rails.application.config.assets.precompile += %w(
+                                                  frontend.css
+                                                  frontend.js
+                                                  messages_en.properties
+                                                  sourcesanspro/SourceSansPro-Regular.ttf
+                                                  fontawesome/fontawesome.woff fontawesome/fontawesome.ttf fontawesome/fontawesome.eot fontawesome/fontawesome.svg
+                                                )
--- components/vis_legacy/app/assets/javascripts/vis_legacy_boot.js
+++
@@ -1,5 +0,0 @@
-//= require_tree ./mixins
-//= require_self
-
-window.chorus.translation_files.push('/assets/visualizations_messages_en.properties');
-window.chorus.views.visualizations = {};
