f98df569d50f59ce61dbee605e040d8122b85107
Kevin Trowbridge <kevinmtrowbridge@gmail.com>
2015-11-12 13:17:03 -0800

    Moving the environments/integration.rb file to trash component.
    #cleanup

--- components/trash/config/environments/integration.rb
+++ components/trash/config/environments/integration.rb
@@ -1,8 +1,9 @@
+# KT TODO: scrub 'integration' environment from app ...
+
 require_relative 'test'
 
 Rails.application.configure do
   config.action_dispatch.show_exceptions = false # make sure capybara server middleware gets exceptions
-  config.middleware.delete(::Rack::Sendfile)
   config.allow_concurrency = true
   config.action_controller.allow_forgery_protection = true
 end
