f7dba76bbfe47eb678edc0a96563b03bb1bff036
Kevin Trowbridge <kevinmtrowbridge@gmail.com>
2015-11-18 12:09:20 -0800

    Keep Chorus compatible with old cookies, for now.
    #componentization #cleanup

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.action_dispatch.cookies_serializer = :json
+# KT TODO: fully switch to JSON for post 5.8 release
+# http://stackoverflow.com/questions/25031985/rails-4-upgrade-jsonparseerror-for-old-sessions
+Rails.application.config.action_dispatch.cookies_serializer = :hybrid
