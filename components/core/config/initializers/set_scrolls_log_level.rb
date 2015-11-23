4a6e50b0db1cd2018c62a6572f91e88dfa2b452f
Michel Milano <mmilano@alpinenow.com>
2015-11-23 15:49:01 -0800

    some map info to scrolls initializer

--- components/core/config/initializers/set_scrolls_log_level.rb
+++ components/core/config/initializers/set_scrolls_log_level.rb
@@ -1,3 +1,14 @@
+#  Scrolls LOG_LEVEL_MAP
+#  "emergency" => 0,
+#  "alert"     => 1,
+#  "critical"  => 2,
+#  "error"     => 3,
+#  "warning"   => 4,
+#  "notice"    => 5,
+#  "info"      => 6,
+#  "debug"     => 7
+
+
 unless ENV["LOG_LEVEL"]
   rails_scrolls_level_map = {
       0 => 7,
