e691833c27abaac90ed8b8ff3417724fbddadff0
Michel Milano <mmilano@alpinenow.com>
2015-11-22 21:08:57 -0800

    incremental work on mailer

--- components/core/app/mailers/mailer.rb
+++ components/core/app/mailers/mailer.rb
@@ -2,7 +2,7 @@ class Mailer < ActionMailer::Base
 
   def notify(user, event)
   
-      email_with_name = '#{user.name} <#{user.email}>'
+    #email_with_name = '#{user.name} <#{user.email}>'
     @user = user
     @job = event.job
     @workspace = event.workspace
@@ -11,19 +11,19 @@ class Mailer < ActionMailer::Base
     
     attachments.inline[as_png('logo')] = logo(License.instance)
     
-    email_image_tag_inline['afm.png', Rails.root.join('public', 'images', 'workfiles', 'icon', 'afm.png')]
-    #attachments[as_png(RunWorkFlowTaskResult.name)] = File.read(Rails.root.join('public', 'images', 'workfiles', 'icon', 'afm.png'))
+    #email_image_tag_inline['afm.png', Rails.root.join('public', 'images', 'workfiles', 'icon', 'afm.png')]
+    attachments[as_png(RunWorkFlowTaskResult.name)] = File.read(Rails.root.join('public', 'images', 'workfiles', 'icon', 'task-afm.png'))
     
 
-    #attachments[as_png(ImportSourceDataTaskResult.name)] = File.read(Rails.root.join('public', 'images', 'jobs', 'task-import.png'))
-    email_image_tag_inline['task-import.png', Rails.root.join('public', 'images', 'jobs', 'task-import.png')]
+    attachments[as_png(ImportSourceDataTaskResult.name)] = File.read(Rails.root.join('public', 'images', 'jobs', 'task-import.png'))
+    #email_image_tag_inline['task-import.png', Rails.root.join('public', 'images', 'jobs', 'task-import.png')]
     
 
     safe_deliver mail(:to => user.email, :subject => event.header)
   end
 
   def chorus_expiring(user, license)
-    email_with_name = '#{user.name} <#{user.email}>'
+    #email_with_name = '#{user.name} <#{user.email}>'
     
     @user = user
     @expiration_date = license[:expires]
@@ -55,44 +55,35 @@ class Mailer < ActionMailer::Base
     Chorus.log_error "#{e.message} : #{e.backtrace}"
   end
 
-    def email_image_tag_inline(image, imagepath, **options)
-        attachments.inline[image] = {
-            :data => File.read(#{imagepath}),
+  def email_image_tag_inline(image, imagepath, **options)
+    attachments.inline[image] = {
+        :data => File.read(#{imagepath}),
+        :mime_type => "image/png",
+        :encoding => "base64"
+    }
+    image_tag attachments.inline[image].url, **options
+  end
+  
+  module MailerHelper
+    def build_backbone_url(path)
+        urls = Rails.configuration.action_mailer.default_url_options
+        "#{urls[:protocol]}://#{urls[:host]}:#{urls[:port]}/##{path}"
+    end
+
+    def as_png(name)
+        %(#{name}.png)
+    end
+
+    def email_image_tag(image, imagepath, **options)
+        attachments[image] = {
+            :data => File.read(Rails.root.join("app/assets/images/emails/#{image}")),
             :mime_type => "image/png",
             :encoding => "base64"
         }
-        image_tag attachments.inline[image].url, **options
+        image_tag attachments[image].url, **options
     end
-
-    module MailerHelper
-        def build_backbone_url(path)
-            urls = Rails.configuration.action_mailer.default_url_options
-            "#{urls[:protocol]}://#{urls[:host]}:#{urls[:port]}/##{path}"
-        end
-
-        def as_png(name)
-            %(#{name}.png)
-        end
-    end
-
-
-    module EmailHelper
-        def email_image_tag(image, imagepath, **options)
-            attachments[image] = {
-                :data => File.read(Rails.root.join("app/assets/images/emails/#{image}")),
-                :mime_type => "image/png",
-                :encoding => "base64"
-            }
-            image_tag attachments[image].url, **options
-        end
-
-    end
-
+  end
 
   helper MailerHelper
   include MailerHelper
-  
-  helper EmailHelper
-  include EmailHelper
-  
 end
