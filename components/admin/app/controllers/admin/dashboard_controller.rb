78ffe1cb6f2a16c30f593b2a81a617dbf3d5cded
Kevin Trowbridge <kevinmtrowbridge@gmail.com>
2015-11-24 13:53:41 -0800

    Adding current "License Information" to new Admin component.
    #componentization #admin_console

--- components/admin/app/controllers/admin/dashboard_controller.rb
+++ components/admin/app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ module Admin
   class DashboardController < AdminController
 
     def index
+      add_breadcrumb "Admin", :root_path
     end
   end
 end
--- components/admin/config/routes.rb
+++ components/admin/config/routes.rb
@@ -1,3 +1,4 @@
 Admin::Engine.routes.draw do
   root to: 'dashboard#index'
+  resource :license, :only => [:show]
 end
