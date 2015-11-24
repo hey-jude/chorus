f57f9f0a95b94361733602ce81555f1e1a4b165c
Michel Milano <mmilano@alpinenow.com>
2015-11-23 16:16:08 -0800

    WIP

--- components/core/app/mailers/mailer.rb
+++ components/core/app/mailers/mailer.rb
@@ -1,27 +1,25 @@
 class Mailer < ActionMailer::Base
 
   def notify(user, event)
-
-    #email_with_name = "#{user.name} <#{user.email}>"
     @user = user
     @job = event.job
     @workspace = event.workspace
     @job_result = event.job_result
     @job_task_results = event.job_result.job_task_results
 
+    #@email_with_name = '#{user.name} <#{user.email}>'
+
     attachments.inline[as_png('logo')] = logo(License.instance)
 
 #   attachments[as_png(RunWorkFlowTaskResult.name)] = File.read(Core::Engine.root.join('public', 'images', 'workfiles', 'icon', 'afm.png'))
     attachments.inline[as_png(RunWorkFlowTaskResult.name)] = File.read(Core::Engine.root.join('public', 'images', 'jobs', 'task-afm.png'))
     attachments.inline[as_png(ImportSourceDataTaskResult.name)] = File.read(Core::Engine.root.join('public', 'images', 'jobs', 'task-import.png'))
-    attachments.inline[as_png(ImportSourceDataTaskResult.name)] = File.read(Core::Engine.root.join('public', 'images', 'jobs', 'task-sql.png'))
+    attachments.inline[as_png(RunSqlWorkfileTask.name)] = File.read(Core::Engine.root.join('public', 'images', 'jobs', 'task-sql.png'))
 
     safe_deliver mail(:to => user.email, :subject => event.header)
   end
 
   def chorus_expiring(user, license)
-    #email_with_name = '#{user.name} <#{user.email}>'
-
     @user = user
     @expiration_date = license[:expires]
     @branding = license.branding
@@ -31,15 +29,6 @@ class Mailer < ActionMailer::Base
 
     safe_deliver mail(:to => user.email, :subject => 'Your Chorus license is expiring.')
   end
-
-#  def notify_worklet_complete(user, event)
-#    email_with_name = '#{user.name} <#{user.email}>'
-#    @user = user
-#    attachments[as_png('logo')] = logo(License.instance)
-#
-#    safe_deliver mail(:to => user.email, :subject => 'Your Touchpoint has completed.')
-#  end
-
 
   private
 
@@ -54,32 +43,14 @@ class Mailer < ActionMailer::Base
     Chorus.log_error "#{e.message} : #{e.backtrace}"
   end
 
-  def email_image_tag_inline(image, imagepath, **options)
-    attachments.inline[image] = {
-        :data => File.read(#{imagepath}),
-        :mime_type => "image/png",
-        :encoding => "base64"
-    }
-    image_tag attachments.inline[image].url, **options
-  end
-
   module MailerHelper
     def build_backbone_url(path)
-        urls = Rails.configuration.action_mailer.default_url_options
-        "#{urls[:protocol]}://#{urls[:host]}:#{urls[:port]}/##{path}"
+      urls = Rails.configuration.action_mailer.default_url_options
+      "#{urls[:protocol]}://#{urls[:host]}:#{urls[:port]}/##{path}"
     end
 
     def as_png(name)
-        %(#{name}.png)
-    end
-
-    def email_image_tag(image, imagepath, **options)
-        attachments[image] = {
-            :data => File.read(Rails.root.join("app/assets/images/emails/#{image}")),
-            :mime_type => "image/png",
-            :encoding => "base64"
-        }
-        image_tag attachments[image].url, **options
+      %(#{name}.png)
     end
   end
 
