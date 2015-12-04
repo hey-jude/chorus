b2316c300f85d6d1249ee7f9032d8c1aa32bd559
Kevin Trowbridge <kevinmtrowbridge@gmail.com>
2015-12-04 15:22:31 -0800

    * Adding Mike Souza's db migration to VisChiasm. * A few edits to VisChiasm58 with Curran's help.
    #chiasm #componentization

--- components/vis_chiasm/lib/vis_chiasm/engine.rb
+++ components/vis_chiasm/lib/vis_chiasm/engine.rb
@@ -1,4 +1,17 @@
 module VisChiasm
   class Engine < ::Rails::Engine
+
+    # http://pivotallabs.com/leave-your-migrations-in-your-rails-engines/
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+
+      ActiveRecord::Tasks::DatabaseTasks.migrations_paths = ActiveRecord::Tasks::DatabaseTasks.migrations_paths | app.config.paths['db/migrate'].to_a
+      ActiveRecord::Migrator.migrations_paths = ActiveRecord::Migrator.migrations_paths | app.config.paths['db/migrate'].to_a
+    end
+
   end
 end
--- components/vis_chiasm_58/app/assets/javascripts/views/data_sources/hdfs_show_file_preview_view.js
+++ components/vis_chiasm_58/app/assets/javascripts/views/data_sources/hdfs_show_file_preview_view.js
@@ -1,15 +1,63 @@
 chorus.views.HdfsShowFilePreview = chorus.views.Base.extend({
     templateName: "hdfs_show_file_preview",
 
-    events: {
+    events: {},
+
+    setup: function () {
     },
 
-    setup: function() {
+    postRender: function () {
+        var dataset_id = 1;
+
+        this.chiasmInit(dataset_id);
     },
 
-    additionalContext: function() {
+    additionalContext: function () {
         return {
             includeHeader: true
         }
+    },
+
+    chiasmInit: function (dataset_id) {
+        if (!this.chiasm) {
+
+            // Details on Alpine's API here: https://github.com/alpinedatalabs/adl/pull/920
+            // http://localhost:9090/alpinedatalabs/api/v1/json/data/datasources/GPDB/airline.airports/rows?fetch=random&max=10&columns=airport,city&token=c2060462434fb03bc12076f202a36720d279ac35
+
+            var chiasm = new ChiasmBundle();
+            this.chiasm = chiasm;
+
+            chiasm.setConfig({
+                "layout": {
+                    "plugin": "layout",
+                    "state": {
+                        "containerSelector": "#chiasm-container",
+                        "layout": "barChart"
+                    }
+                },
+                "barChart": {
+                    "plugin": "barChart",
+                    "state": {
+                        "sizeColumn": "age",
+                        "idColumn": "name",
+                        "fill": "none",
+                        "stroke": "black",
+                        "strokeWidth": "2px",
+                        "barPadding": 0.2
+                    }
+                }
+            });
+
+            var data = [
+                {name: "Joe", age: 29, birthday: new Date(1986, 11, 17)},
+                {name: "Jane", age: 31, birthday: new Date(1985, 1, 15)}
+            ];
+
+            chiasm.getComponent("barChart").then(function (barChart) {
+                console.log(barChart);
+                barChart.data = data;
+            });
+        }
     }
+
 });
